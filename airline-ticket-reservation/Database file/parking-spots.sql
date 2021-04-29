fc7e883e954538497ffa50d704f670380876a3c0
Hana Awad <awadhana0825@gmail.com>
2021-04-29 03:06:26 -0400

    Add files via upload

--- airline-ticket-reservation/Database file/parking-spots.sql
+++ airline-ticket-reservation/Database file/parking-spots.sql
@@ -93,116 +93,26 @@ INSERT INTO `customer` (`customer_id`, `pwd`, `name`, `email`, `phone_no`, `addr
 -- Table structure for table `flight_details`
 --
 
-CREATE TABLE `flight_details` (
-  `spot_no` varchar(10) NOT NULL,
-  `departure_date` date NOT NULL,
-  `arrival_date` date DEFAULT NULL,
+CREATE TABLE `parking_details` (
+  `type` varchar(15) NOT NULL,
+  -- `departure_date` date NOT NULL,
+  -- `arrival_date` date DEFAULT NULL,
   `spots_regular` int(5) DEFAULT NULL,
-  `spots_VIP` int(5) DEFAULT NULL,
-  `price_regular` int(10) DEFAULT NULL,
-  `price_VIP` int(10) DEFAULT NULL,
+  `available` int(5) DEFAULT NULL,
+  `price` int(5) DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `parking_details`
 --
 
-INSERT INTO `parking_details` (`spot_no`, 'departure_date`, `arrival_date`, `spots_regular`, `spots_VIP`, `price_regular`, `price_VIP`) VALUES
-('AA101', 195, 96, 5000, 7500, '10001'),
-('AA102', 200, 73, 2500, 3000, '10002'),
-('AA103', 150, 75, 1200, 1500, '10004'),
-('AA104', 37, 4, 500, 750, '10003'),
-('AA106', 150, 75, 3000, 3750, '10004');
+INSERT INTO `parking_details` (`type`,`spot_no`, `available`, 'price`) VALUES
+('VIP', 'PA101', 1, 75, '10001'),
+('VIP', 'PA102', 1, 75, '10002'),
+('REGULAR', 'PA103', 1, 40, '10004'),
+('REGULAR', 'PA104', 1, 40, '10003'),
+('REGULAR','PA106', 1, 40, '10004');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `frequent_flier_details`
---
-
-CREATE TABLE `frequent_flier_details` (
-  `frequent_flier_no` varchar(20) NOT NULL,
-  `customer_id` varchar(20) DEFAULT NULL,
-  `mileage` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `frequent_flier_details`
---
-
-INSERT INTO `frequent_flier_details` (`frequent_flier_no`, `customer_id`, `mileage`) VALUES
-('10001000', 'aadith', 375),
-('20002000', 'harryroshan', 150);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jet_details`
---
-
-CREATE TABLE `jet_details` (
-  `jet_id` varchar(10) NOT NULL,
-  `jet_type` varchar(20) DEFAULT NULL,
-  `total_capacity` int(5) DEFAULT NULL,
-  `active` varchar(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jet_details`
---
-
-INSERT INTO `jet_details` (`jet_id`, `jet_type`, `total_capacity`, `active`) VALUES
-('10001', 'Dreamliner', 300, 'Yes'),
-('10002', 'Airbus A380', 275, 'Yes'),
-('10003', 'ATR', 50, 'Yes'),
-('10004', 'Boeing 737', 225, 'Yes'),
-('10007', 'Test_Model', 250, 'Yes');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `passengers`
---
-
-CREATE TABLE `passengers` (
-  `passenger_id` int(10) NOT NULL,
-  `pnr` varchar(15) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `age` int(3) DEFAULT NULL,
-  `gender` varchar(8) DEFAULT NULL,
-  `meal_choice` varchar(5) DEFAULT NULL,
-  `frequent_flier_no` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `passengers`
---
-
-INSERT INTO `passengers` (`passenger_id`, `pnr`, `name`, `age`, `gender`, `meal_choice`, `frequent_flier_no`) VALUES
-(1, '1669050', 'Harry Roshan', 20, 'male', 'yes', '20002000'),
-(1, '2369143', 'blah', 20, 'male', 'yes', NULL),
-(1, '3027167', 'aadith_name', 10, 'male', 'yes', NULL),
-(1, '3773951', 'harry', 51, 'male', 'yes', NULL),
-(1, '4797983', 'pass1', 34, 'male', 'yes', NULL),
-(1, '5421865', 'pass1', 10, 'male', 'yes', NULL),
-(1, '6980157', 'roshan', 20, 'male', 'yes', NULL),
-(1, '8503285', 'aadith_name', 10, 'male', 'yes', '10001000'),
-(2, '1669050', 'berti harry', 45, 'female', 'yes', NULL),
-(2, '2369143', 'blah', 51, 'male', 'yes', NULL),
-(2, '3027167', 'roshan', 20, 'male', 'yes', NULL),
-(2, '3773951', 'berti', 42, 'female', 'yes', NULL),
-(2, '4797983', 'Harry Roshan', 20, 'male', 'yes', '20002000'),
-(2, '5421865', 'pass2', 20, 'female', 'yes', NULL),
-(2, '6980157', 'aadith', 9, 'male', 'yes', NULL),
-(2, '8503285', 'roshan_name', 20, 'male', 'yes', NULL),
-(3, '1669050', 'aadith_name', 10, 'male', 'yes', NULL),
-(3, '2369143', 'blah', 10, 'male', 'yes', NULL),
-(3, '3773951', 'aadith', 11, 'male', 'yes', '10001000'),
-(3, '4797983', 'aadith_name', 10, 'male', 'yes', '10001000'),
-(3, '5421865', 'pass3', 30, 'male', 'yes', NULL),
-(4, '2369143', 'blah', 42, 'female', 'yes', NULL);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `payment_details`
@@ -223,18 +133,16 @@ CREATE TABLE `payment_details` (
 INSERT INTO `payment_details` (`payment_id`, `pnr`, `payment_date`, `payment_amount`, `payment_mode`) VALUES
 ('120000248', '4797983', '2017-11-25', 4200, 'credit card'),
 ('142539461', '3773951', '2017-11-25', 4050, 'credit card'),
-('165125569', '8503285', '2017-11-25', 7500, 'net banking'),
 ('467972527', '2369143', '2017-11-26', 33400, 'debit card'),
 ('557778944', '6980157', '2017-11-26', 11700, 'credit card'),
 ('620041544', '1669050', '2017-11-25', 4800, 'debit card'),
-('665360715', '5421865', '2017-11-28', 15750, 'net banking'),
 ('862686553', '3027167', '2017-11-25', 10700, 'debit card');
 
 --
 -- Triggers `payment_details`
 --
 DELIMITER $$
-CREATE TRIGGER `update_ticket_after_payment` AFTER INSERT ON `payment_details` FOR EACH ROW UPDATE ticket_details
+CREATE TRIGGER `update_ticket_after_payment` AFTER INSERT ON `payment_details` FOR EACH ROW UPDATE parking_details
      SET booking_status='CONFIRMED', payment_id= NEW.payment_id
    WHERE pnr = NEW.pnr
 $$
@@ -265,7 +173,7 @@ CREATE TABLE `ticket_details` (
 -- Dumping data for table `ticket_details`
 --
 
-INSERT INTO `ticket_details` (`pnr`, `date_of_reservation`, `flight_no`, `journey_date`, `class`, `booking_status`, `no_of_passengers`, `lounge_access`, `priority_checkin`, `insurance`, `payment_id`, `customer_id`) VALUES
+INSERT INTO `ticket_details` (`pnr`, `date_of_reservation`, `spot_no`, `journey_date`, `class`, `booking_status`, `no_of_passengers`, `lounge_access`, `priority_checkin`, `insurance`, `payment_id`, `customer_id`) VALUES
 ('1669050', '2017-11-25', 'AA104', '2017-12-04', 'business', 'CONFIRMED', 3, 'yes', 'yes', 'yes', '620041544', 'harryroshan'),
 ('2369143', '2017-11-26', 'AA101', '2017-12-01', 'business', 'CONFIRMED', 4, 'yes', 'yes', 'yes', '467972527', 'blah'),
 ('3027167', '2017-11-25', 'AA101', '2017-12-01', 'economy', 'CONFIRMED', 2, 'no', 'no', 'yes', '862686553', 'aadith'),
@@ -298,26 +206,6 @@ ALTER TABLE `flight_details`
   ADD PRIMARY KEY (`flight_no`,`departure_date`),
   ADD KEY `jet_id` (`jet_id`);
 
---
--- Indexes for table `frequent_flier_details`
---
-ALTER TABLE `frequent_flier_details`
-  ADD PRIMARY KEY (`frequent_flier_no`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `jet_details`
---
-ALTER TABLE `jet_details`
-  ADD PRIMARY KEY (`jet_id`);
-
---
--- Indexes for table `passengers`
---
-ALTER TABLE `passengers`
-  ADD PRIMARY KEY (`passenger_id`,`pnr`),
-  ADD KEY `pnr` (`pnr`),
-  ADD KEY `frequent_flier_no` (`frequent_flier_no`);
 
 --
 -- Indexes for table `payment_details`
@@ -340,37 +228,17 @@ ALTER TABLE `ticket_details`
 -- Constraints for dumped tables
 --
 
---
--- Constraints for table `flight_details`
---
-ALTER TABLE `flight_details`
-  ADD CONSTRAINT `flight_details_ibfk_1` FOREIGN KEY (`jet_id`) REFERENCES `jet_details` (`jet_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `frequent_flier_details`
---
-ALTER TABLE `frequent_flier_details`
-  ADD CONSTRAINT `frequent_flier_details_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `passengers`
---
-ALTER TABLE `passengers`
-  ADD CONSTRAINT `passengers_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `passengers_ibfk_2` FOREIGN KEY (`frequent_flier_no`) REFERENCES `frequent_flier_details` (`frequent_flier_no`) ON UPDATE CASCADE;
-
---
 -- Constraints for table `payment_details`
 --
 ALTER TABLE `payment_details`
-  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `ticket_details` (`pnr`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`pnr`) REFERENCES `parking_details` (`pnr`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `ticket_details`
 --
-ALTER TABLE `ticket_details`
-  ADD CONSTRAINT `ticket_details_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ticket_details_ibfk_3` FOREIGN KEY (`flight_no`,`journey_date`) REFERENCES `flight_details` (`flight_no`, `departure_date`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `parking_details`
+  ADD CONSTRAINT `parking_details_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `parking_details_ibfk_3` FOREIGN KEY (`flight_no`,`journey_date`) REFERENCES `flight_details` (`flight_no`, `departure_date`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
