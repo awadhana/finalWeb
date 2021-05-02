07bc392ec289beaa69a6031cbbba818ac0a15563
suzannealexander <78283401+suzannealexander@users.noreply.github.com>
2021-05-02 14:38:42 -0400

    Update admin_view_booked_tickets.php

--- airline-ticket-reservation/admin_view_booked_tickets.php
+++ airline-ticket-reservation/admin_view_booked_tickets.php
@@ -7,10 +7,16 @@
 			View Booked Tickets
 		</title>
 		<style>
+		body{
+				background-image: url('slide1.jpeg');
+				text-align:center;
+			}
 			input {
     			border: 1.5px solid #030337;
     			border-radius: 4px;
-    			padding: 7px 30px;
+			padding: 7px 30px;}
+			table,tr,td{
+				margin:auto;
 			}
 			input[type=submit] {
 				background-color: #030337;
@@ -25,19 +31,21 @@
     			padding: 5.5px 44.5px;
 			}
 		</style>
-		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" type="text/css" href="main.css"/>
 		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
 	</head>
 	<body>
-		<img class="logo" src="images/shutterstock_22.jpg"/> 
-		<h1 id="title">
-			AADITH AIRLINES
-		</h1>
 		<div>
-			<ul>
+			<ul class = "menu">
+					<li id= "one">
+				
+						
+					<ul>
 				<li><a href="admin_homepage.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
 				<li><a href="admin_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
 				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+				</ul>
+			</li>
 			</ul>
 		</div>
 		<form action="admin_view_booked_tickets_form_handler.php" method="post">
@@ -55,7 +63,7 @@
 			</table>
 			<br>
 			<br>
-			<input type="submit" value="Submit" name="Submit">
+			<input type="submit" value="SUBMIT" name="Submit">
 			</div>
 		</form>
 	</body>
