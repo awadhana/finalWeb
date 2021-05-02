e2f7c78da9e65832d60d3c34a64aa516251417cb
suzannealexander <78283401+suzannealexander@users.noreply.github.com>
2021-05-02 11:15:48 -0400

    Update home_page.php

--- airline-ticket-reservation/home_page.php
+++ airline-ticket-reservation/home_page.php
@@ -4,20 +4,25 @@
 <html>
 	<head>
 		<title>
-			Welcome to Aadith Airlines
+			JKASH Airlines
 		</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" type="text/css" href="main.css"/>
 		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
 	</head>
 	<body>
-		<img class="logo" src="images/shutterstock_22.jpg"/> 
-		<h1 id="title">
-			AADITH AIRLINES
-		</h1>
-		<div>
+
+<div class = "container">
+		<div class="fling-minislide">
+  <img src="slide4.jpg" alt="Slide 4" />
+  <img src="slide3.jpg" alt="Slide 3" />
+  <img src="slide2.jpg" alt="Slide 2" />
+  <img src="slide1.jpeg" alt="Slide 1" />
+</div>
+		<div id  = "menuicon">
+		
 			<ul>
 				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-				<li>
+			
 					<?php
 						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
 						{
@@ -33,8 +38,7 @@
 						}
 					?>
 				</li>
-				<li><a href="home_page.php"><i class="fa fa-plane" aria-hidden="true"></i> About Us</a></li>
-				<li><a href="home_page.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact Us</a></li>
+				<li><a href="aboutus.php"><i class="fa fa-plane" aria-hidden="true"></i> About Us</a></li>
 				<li>
 					<?php
 						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
@@ -53,13 +57,6 @@
 				</li>
 			</ul>
 		</div>
-		<div class="container">
-			<div class="welcome_text">Welcome to Aadith Airlines!</div>
-			<img src="images/shutterstock_1.jpg" width=100%>
-		</div>
-		<!--check out addling local host in links and other places
-
-			shift login/logout buttons to right side
-		-->
+	</div>
 	</body>
 </html>
