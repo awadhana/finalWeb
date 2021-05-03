a8aa18273c9c10ca31f89575a2cdcfbc4be564bd
suzannealexander <78283401+suzannealexander@users.noreply.github.com>
2021-05-02 20:32:07 -0400

    Add files via upload

--- airline-ticket-reservation/aboutus.php
+++ airline-ticket-reservation/aboutus.php
@@ -53,21 +53,21 @@
 	
 		<br>
 		<h1> About Us </h1>
-		   <p><b>Hana Awad </b>- Project Lead. Ensured project worked cohesively and all game functionality was present.<br>
-      Worked exclusively on buttons relating to the generation as well as bringing cells to life/death with a mouse click</p>
-      <p><b>Adhil Habib-</b> Back end engineer. Collaborated closely with Hana to ensure project meet requirements.<br>
-        Worked exclusively on all patterns, the reset button and dynamic grid sizing.
+		   <p><b>Hana Awad </b>- Project Lead. Ensured project worked cohesively and all database functionality was present.<br>
+			Worked on reserving parking spots with Adhil and databases with Kevin</p>
+      <p><b>Adhil Habib-</b> Back end engineer. Collaborated closely with Hana to create the database and the parking reservation functionality.<br>
+				Worked exclusively on creating Admin roles on website.
         </p>
-      <p><b>Suzanne Alexander</b>-Front end engineer oversaw the front-end functionality of the landing page specifically.<br>
-        Pair-programmed with Hana to ensure back-end functionality was present.
+      <p><b>Suzanne Alexander</b>-Front end engineer oversaw the front-end functionality across all pages.<br>
+        Pair-programmed with Hana to ensure back-end functionality was present as well.
       <br> Worked closely with Kevin and John to ensure project flow was present between pages</p>
         <p><b>Kevin Nguyen</b>- Front end engineer, working closely with Suzanne to ensure project success. <br>
         Project deck designer and UML diagram creator also pair-programming with Hana to ensure back-end<br>
       functionality was present.</p>
-      <p><b>John Arthur</b>- Front end enginner. Worked on log in page, authentification system and front-end <br>
+      <p><b>John Arthur</b>- Front end enginner. Worked on payment pages, authentification system and front-end <br>
       design alongside Suzanne and Kevin.</p>
 
-      <p><b> Description: </b>Conway's Game of Life with extensive functionality.</p>
+      <p><b> Description: </b>JKASH Transportation: Booking flights and reserving parking spots</p>
 
       <p> <b>Methodology:</b> We used the Agile Scrum framework. Thanks to it's flexible design and <br>
       the ease in implementation with continuous reiteration and reevealuation of priorities, the <br>
--- airline-ticket-reservation/admin_ticket_message.php
+++ airline-ticket-reservation/admin_ticket_message.php
@@ -28,7 +28,7 @@
 			<ul>
 				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
 				<li><a href="customer_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
-				<li><a href="home_page.php"><i class="fa fa-address-book" aria-hidden="true"></i> About Us</a></li>
+				<li><a href="aboutus.php"><i class="fa fa-address-book" aria-hidden="true"></i> About Us</a></li>
 				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
 			</ul>
 		</div>
