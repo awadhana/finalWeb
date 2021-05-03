10827313c6c8633df38a9f000c5cd3f7a3c520ec
Hana Awad <awadhana0825@gmail.com>
2021-05-03 19:33:00 -0400

    Add files via upload

--- airline-ticket-reservation/book_parking_payment.php
+++ airline-ticket-reservation/book_parking_payment.php
@@ -16,7 +16,7 @@
 				text-align:center;
 		}
 		html{
-			  background: url(slide5.jpg) no-repeat center center fixed; 
+			  background: url(slide5.jpg) no-repeat center center fixed;
 			  -webkit-background-size: cover;
 			  -moz-background-size: cover;
 			  -o-background-size: cover;
@@ -34,14 +34,8 @@
     			padding: 7px 45px;
     			margin: 0px 357px
 			}
-			table,tr,td{
-				margin:auto;
-			}
-			td{
-				background-color:#929199;
-			}
 		</style>
-	
+
 	</head>
 	<body>
 		<div>
@@ -52,40 +46,38 @@
 				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
 			</ul>
 		</div>
-		<h3> WE ACCEPT </h3>
-		<img src="creditcard.jpg" style="width:400px;height:60px;">
 		<form action="parking_payment_details_form_handler.php" method="post">
 			<h2>ENTER THE PAYMENT DETAILS</h2>
-			<h3 style="margin:auto"><u>Payment Summary</u></h3>
+			<h3 style="margin-left: 30px"><u>Payment Summary</u></h3>
 			<?php
 				$parking_no=$_POST['select_parking'];
 				$payment_id=rand(100000000,999999999);
 				$pnr=$_SESSION['pnr'];
 				$_SESSION['payment_id']=$payment_id;
-				$payment_date=date('Y-m-d'); 
+				$payment_date=date('Y-m-d');
 				$_SESSION['payment_date']=$payment_date;
 				$_SESSION['select_parking'] = $parking_no;
 				$days_no = $_POST['no_of_days'];
 
 				require_once('Database Connection file/mysqli_connect.php');
-				
+
 					$query="SELECT price FROM parking_details where spot_no=?";
 					$stmt=mysqli_prepare($dbc,$query);
 					mysqli_stmt_bind_param($stmt,"s",$parking_no);
 					mysqli_stmt_execute($stmt);
 					mysqli_stmt_bind_result($stmt,$ticket_price);
 					mysqli_stmt_fetch($stmt);
-				
-				
+
+
 				mysqli_stmt_close($stmt);
 				mysqli_close($dbc);
 				$total_ticket_price=$ticket_price *$days_no;
-				
-				
+
+
 				$total_amount=$total_ticket_price;
 				$_SESSION['total_amount']=$total_amount;
 
-				echo "<table cellpadding=\"5\"	style='margin: auto'>";
+				echo "<table cellpadding=\"5\"	style='margin-left: 50px'>";
 				echo "<tr>";
 				echo "<td class=\"fix_table\">Parking Fee:</td>";
 				echo "<td class=\"fix_table\">&#x24; ".$total_ticket_price."</td>";
@@ -93,160 +85,31 @@
 
 				echo "</table>";
 
-				echo "<hr style='margin-right:900px; margin:auto'>";
-				echo "<table cellpadding=\"5\" style='margin: auto'>";
+				echo "<hr style='margin-right:900px; margin-left: 50px'>";
+				echo "<table cellpadding=\"5\" style='margin-left: 50px'>";
 				echo "<tr>";
 				echo "<td class=\"fix_table\"><strong>Total:</strong></td>";
 				echo "<td class=\"fix_table\">&#x24; ".$total_amount."</td>";
 				echo "</tr>";
 				echo "</table>";
-				echo "<hr style='margin-right:900px; margin:auto'>";
+				echo "<hr style='margin-right:900px; margin-left: 50px'>";
 				echo "<br>";
 				echo "<p style=\"margin-left:50px\">Your Payment/Transaction ID is <strong>".$payment_id.".</strong> Please note it down for future reference.</p>";
 				echo "<br>";
 			?>
-	<input style='margin: auto'type="submit" value="PAY NOW" name="Pay_Now">
-		
-		</form>
-<table cellpadding="5" style='margin: auto'>
+			<table cellpadding="5" style='margin-left: 50px'>
 				<tr>
-					<caption>ENTER THE PAYMENT MODE:</caption>
+					<td class="fix_table"><strong>Enter the Payment Mode:-</strong></td>
 				</tr>
 				<tr>
 					<td class="fix_table"><i class="fa fa-credit-card" aria-hidden="true"></i> Credit Card <input type="radio" name="payment_mode" value="credit card" checked></td>
 					<td class="fix_table"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Debit Card <input type="radio" name="payment_mode" value="debit card"></td>
+					<td class="fix_table"><i class="fa fa-desktop" aria-hidden="true"></i> Net Banking <input type="radio" name="payment_mode" value="net banking"></td>
 				</tr>
 			</table>
-			<div class="payment_details">
 			<br>
-			<dl style="margin:auto">
-
-				<table>
-				<tr>
-					<td>
-						<div style="float:left;">
-				<label for = "card_number">Card Number</label>
-						<input id = "card_number"class="input" type="text" name="card_number" value="" required><br>
-				</div>
-			</td>
-		<td>	<div class="well" >
-			Please, type some card number above.
-		</div>
-		</td>
-		<br>
+			<input type="submit" value="Pay Now" name="Pay_Now">
 		</form>
-
-	</div>
-			<td style = "padding-left: 35px;" >
-				<div style="float:right;">
-		<label for = "s_code">Security Code</label>
-				<input id = "s_code"class="input" type="password" name="s_code" value="" maxlength="4" size="1" required><br>
-		</div>
-
-			</td>
-				</tr>
-
-				<tr>
-					<td>
-					<div style="float:left;">
-					<label for = "UName">Name on Card</label>
-					<input type="text" id="UName" name="u_name" required><br>
-	 				</div>
-
-				</td>
-
-				<td style="display:inline-block">
-				<div style="float:right">
-				<label for = "e_date">Card Expiration</label>
-				<br>
-				<select name="month" id="ExpDateM" value = "MM" >
-	 			<option value="01">01</option>
-	 			<option value="02">02</option>
-	 			<option value="03">03</option>
-				<option value="04">04</option>
-				<option value="05">05</option>
-				<option value="06">06</option>
-				<option value="07">07</option>
-				<option value="08">08</option>
-				<option value="09">09</option>
-				<option value="10">10</option>
-				<option value="11">11</option>
-			 	<option value="12">12</option>
- </select>
- 
-	<select name="Year" id="ExpDateY" value = "YY" >
-	 <option value="21">2021</option>
-	 <option value="02">2022</option>
-	 <option value="03">2023</option>
-	 <option value="04">2024</option>
-	 <option value="05">2025</option>
-	 <option value="06">2026</option>
-	 <option value="07">2027</option>
-	 <option value="08">2028</option>
-	 <option value="09">2029</option>
-	 <option value="10">2030</option>
-	 <option value="11">2031</option>
-	 <option value="12">2032</option>
-</select>
-				</div>
-
-			</td>
-
-				</tr>
-					<tr></tr>
-				<tr>
-					<td>
-					<div >
-					<label for = "sAddress">Shipping Address</label>
-					<input type="text" id="s_Address" name="s_Address" required><br>
-					</div>
-				</td>
-				</tr>
-				<tr></tr>
-				<tr>
-					<td>
-					<div >
-					<label for = "bAddress">Billing Address</label>
-					<input type="text" id="b_Address" name="b_Address" required><br>
-					</div>
-				</td>
-				</tr>
-				<tr></tr>
-				<tr>
-					<td>
-					<div >
-					<label for = "pNumber">Phone Number</label>
-					<input type="text" id="pNumber" name="p_Number" maxlength="11" required><br>
-					</div>
-				</td>
-				</tr>
-
-			</table>
-		
-
-				<!-- <div class="form-group">
-					<input class="input" type="text" placeholder="Type your card number">
-				</div> -->
-
-
-
-	<script src="dist/card-type.js"></script>
-	<script>
-		var input = document.querySelector('.input')
-		var result = document.querySelector('.well')
-		// var hidden = document.getElementById('hidden')
-
-		input.addEventListener('change', onInputChange)
-		input.addEventListener('input', onInputChange)
-
-		function onInputChange (e) {
-			var cards = CardType.cardType(e.target.value)
-			result.innerHTML = JSON.stringify(cards)
-			// hidden.value = JSON.stringify(cards)
-		}
-	</script>
-	<br>
-	
 
 	</body>
 </html>
--- airline-ticket-reservation/parking_payment_details_form_handler.php
+++ airline-ticket-reservation/parking_payment_details_form_handler.php
@@ -1,95 +1,45 @@
-  
 <?php
 	session_start();
 ?>
 <html>
 	<head>
 		<title>Submit Payment Details</title>
-						<link rel="stylesheet" type="text/css" href="main.css"/>
-		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
-	<style>
-			a{
-			font-size:25px;
-		}
-				body{
-				background-image: url('slide1.jpeg');
-				text-align:center;
-				}
-			</style>
 	</head>
 	<body>
-	<div>
-			<ul class = "menu">
-					<li id= "one">
-				
-						
-					<ul>
-				<li><a href="admin_homepage.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-				<li><a href="admin_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
-				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
-				</ul>
-			</li>
-			</ul>
-		</div>
-	<br><br><br><br><br>
-	
-	<h3> Your payment was successful </h3>
 		<?php
-		/*
 			if(isset($_POST['Pay_Now']))
 			{
-				$no_of_pass=$_SESSION['no_of_days'];
-				$flight_no=$_SESSION['parking_no'];
-			//	$journey_date=$_SESSION['journey_date'];
-				$class=$_SESSION['class'];
+				$parking_no=$_SESSION['select_parking'];
 				$pnr=$_SESSION['pnr'];
 				$payment_id=$_SESSION['payment_id'];
 				$total_amount=$_SESSION['total_amount'];
 				$payment_date=$_SESSION['payment_date'];
-				$payment_mode=$_POST['payment_mode'];
-				$card_number=$_POST['card_number'];
-				$card_name=$_POST['u_name'];
-				$billing_address=$_POST['b_Address'];
-				$shipping_address=$_POST['s_Address'];
-				$phone_number=$_POST['p_Number'];
-				$security_code=$_POST['s_code'];
-				$Exp_month=$_POST['month'];
-				$Exp_year=$_POST['Year'];
-				// $card_type=$_POST['card_type'];
+				$payment_mode=$_POST['payment_mode'];				
+echo $parking_no;
 				require_once('Database Connection file/mysqli_connect.php');
-			/*	if($class=='economy')
-				{
-					$query="UPDATE Flight_Details SET seats_economy=seats_economy-? WHERE flight_no=? AND departure_date=?";
+				
+					$query="UPDATE parking_details SET available=0 WHERE spot_no=?";
 					$stmt=mysqli_prepare($dbc,$query);
-					mysqli_stmt_bind_param($stmt,"iss",$no_of_pass,$flight_no,$journey_date);
+					mysqli_stmt_bind_param($stmt,"s",$parking_no);
 					mysqli_stmt_execute($stmt);
 					$affected_rows_1=mysqli_stmt_affected_rows($stmt);
 					echo $affected_rows_1.'<br>';
 					mysqli_stmt_close($stmt);
-				}
-				else if($class=='business')
-				{
-					$query="UPDATE Flight_Details SET seats_business=seats_business-? WHERE flight_no=? AND departure_date=?";
-					$stmt=mysqli_prepare($dbc,$query);
-					mysqli_stmt_bind_param($stmt,"iss",$no_of_pass,$flight_no,$journey_date);
-					mysqli_stmt_execute($stmt);
-					$affected_rows_1=mysqli_stmt_affected_rows($stmt);
-					echo $affected_rows_1.'<br>';
-					mysqli_stmt_close($stmt);
-				}*/
+				
+				
 				// mysqli_stmt_bind_result($stmt,$cnt);
 				// mysqli_stmt_fetch($stmt);
 				// echo $cnt;
 				/*
 				$response=@mysqli_query($dbc,$query);
-				
+				*/
 				if($affected_rows_1==1)
 				{
 					echo "Successfully Updated Seats<br>";
 
-					$query="INSERT INTO Payment_Details (payment_id,pnr,payment_date,payment_amount,payment_mode,card_name,card_number,billing_address,phone_number, shipping_address, security_code) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+					$query="INSERT INTO Payment_Details (payment_id,pnr,payment_date,payment_amount,payment_mode) VALUES (?,?,?,?,?)";
 					$stmt=mysqli_prepare($dbc,$query);
-					mysqli_stmt_bind_param($stmt,"sssissssisi",$payment_id,$pnr,$payment_date,$total_amount,$payment_mode,$card_name, $card_number, $billing_address,$phone_number,$shipping_address, $security_code);
+					mysqli_stmt_bind_param($stmt,"sssis",$payment_id,$pnr,$payment_date,$total_amount,$payment_mode);
 					mysqli_stmt_execute($stmt);
 					$affected_rows_2=mysqli_stmt_affected_rows($stmt);
 					echo $affected_rows_2.'<br>';
@@ -115,7 +65,7 @@
 			else
 			{
 				echo "Payment request not received";
-			}*/
+			}
 		?>
 	</body>
 </html>
