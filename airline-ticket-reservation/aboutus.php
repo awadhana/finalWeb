41275bee7410584e087a7c4e2b8d0515d566646c
suzannealexander <78283401+suzannealexander@users.noreply.github.com>
2021-05-03 10:10:07 -0400

    Update aboutus.php

--- airline-ticket-reservation/aboutus.php
+++ airline-ticket-reservation/aboutus.php
@@ -54,7 +54,7 @@
 		<br>
 		<h1> About Us </h1>
 		   <p><b>Hana Awad </b>- Project Lead. Ensured project worked cohesively and all database functionality was present.<br>
-			Worked on reserving parking spots with Adhil and databases with Kevin</p>
+			Worked on reserving parking spots with Adhil and databases with Kevin and UML diagram creator</p>
       <p><b>Adhil Habib-</b> Back end engineer. Collaborated closely with Hana to create the database and the parking reservation functionality.<br>
 				Worked exclusively on creating Admin roles on website.
         </p>
@@ -62,9 +62,9 @@
         Pair-programmed with Hana to ensure back-end functionality was present as well.
       <br> Worked closely with Kevin and John to ensure project flow was present between pages</p>
         <p><b>Kevin Nguyen</b>- Front end engineer, working closely with Suzanne to ensure project success. <br>
-        Project deck designer and UML diagram creator also pair-programming with Hana to ensure back-end<br>
+        Project designer, also pair-programming with Hana to ensure back-end<br>
       functionality was present.</p>
-      <p><b>John Arthur</b>- Front end enginner. Worked on payment pages, authentification system and front-end <br>
+      <p><b>John Arthur</b>- Front end engineer. Worked on payment pages, authentification system and front-end <br>
       design alongside Suzanne and Kevin.</p>
 
       <p><b> Description: </b>JKASH Transportation: Booking flights and reserving parking spots</p>
@@ -73,7 +73,7 @@
       the ease in implementation with continuous reiteration and reevealuation of priorities, the <br>
     the team was able to seamlessly coordinate between each other with clear and distinct roles. The <br>
     framework allowed us to implement sprints where we continuously gave input on project status, issues and, <br>
-  most importantly, successes. Thanks to this Methodology, we were able to complete the entirety of our project<br>
+  most importantly, successes. Thanks to this methodology, we were able to complete the entirety of our project<br>
 within half the alloted time for project submission.</p>
 
 		
